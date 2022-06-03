8b3a8926438f427c499672aae2fb057a765f0b03
i-timur <i.timur@mail.ru>
2022-06-03 22:11:17 +0300

    added loader

--- src/components/HotNews/HotNews.tsx
+++ src/components/HotNews/HotNews.tsx
@@ -1,50 +1,10 @@
 import {FC} from 'react';
-import {Link} from 'react-router-dom';
 
-import {hf, elec, ww, fish, vert} from '../../assets/images';
 import {Post} from '../../interfaces';
 
 import {HotNewsPreview} from './components/index';
 
 import './HotNews.scss';
-
-const hotNews = [
-  {
-    id: 0,
-    title: 'Entity стала чемпионом Winline Dota 2 Champions League Season 8',
-    category: 'Статьи',
-    commentsCount: 22,
-    image: fish
-  },
-  {
-    id: 1,
-    title: 'Впервые сыграл в оригинальную Half-Life — как культовый шутер Valve ощущается спустя 23 года после релиза',
-    category: 'Статьи',
-    commentsCount: 22,
-    image: hf
-  },
-  {
-    id: 2,
-    title: 'День рождения худшей карты в истории CS:GO — за что почти все ненавидят Vertigo',
-    category: 'Статьи',
-    commentsCount: 22,
-    image: vert
-  },
-  {
-    id: 3,
-    title: 'Матч дня: Astralis против Natus Vincere — в 20:30 мск',
-    category: 'Статьи',
-    commentsCount: 22,
-    image: elec
-  },
-  {
-    id: 4,
-    title: 'Обзор Weird West — безумный вестерн с оборотнями, ведьмами и привкусом Prey',
-    category: 'Статьи',
-    commentsCount: 22,
-    image: ww
-  },
-];
 
 interface Props {
   posts: Post[];
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import {FC, useEffect, useState} from 'react';
+import {Circles} from 'react-loader-spinner';
 
 import {HotNews, MainNews, HomeCards} from '../../components';
 import {Post} from '../../interfaces';
@@ -9,6 +10,7 @@ import './Home.scss';
 
 const Home: FC = () => {
   const [posts, setPosts] = useState<Post[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     http.get('posts.json')
@@ -22,27 +24,43 @@ const Home: FC = () => {
           })
           .sort(comparePosts);
         setPosts(posts);
+        setLoading(false);
       });
   }, []);
 
   return (
     <main className="home">
       <div className="home__container">
-        <section className="home__hot-news hot-news-home">
-          {posts && <HotNews posts={posts} />}
-        </section>
-        <section className="home__main main-home">
-          <div className="main-home__container container">
-            <div className="main-home__left">
-              {posts && <MainNews posts={posts.filter((post) => post.postType.value === 'news')} />}
-            </div>
-            <div className="main-home__right">
-              <div className="main-home__cards">
-                {posts && <HomeCards posts={posts.filter((post) => post.postType.value !== 'news')} />}
-              </div>
+        {loading ? (
+          <div className="home__loader">
+            <div className="home__loader-container">
+              <Circles
+                height="100"
+                width="100"
+                color="#EE773B"
+                ariaLabel="loading"
+              />
             </div>
           </div>
-        </section>
+          ) : (
+          <>
+            <section className="home__hot-news hot-news-home">
+              {posts && <HotNews posts={posts} />}
+            </section>
+            <section className="home__main main-home">
+              <div className="main-home__container container">
+                <div className="main-home__left">
+                  {posts && <MainNews posts={posts.filter((post) => post.postType.value === 'news')} />}
+                </div>
+                <div className="main-home__right">
+                  <div className="main-home__cards">
+                    {posts && <HomeCards posts={posts.filter((post) => post.postType.value !== 'news')} />}
+                  </div>
+                </div>
+              </div>
+            </section>
+          </>
+        )}
       </div>
     </main>
   );
